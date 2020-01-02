b8bd07ff7cfbfd4eda100e3af7b55fb9575680dc
Mohammad Mahmud Hasan <34007876+mahmud92542@users.noreply.github.com>
2020-01-02 16:30:08 +0600

    Update footer.php

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 
     <div id="copyright">
       <p class="fl_left">Copyright &copy; 2019 - All Rights Reserved | <a href="adminlogin.php">Admin Login Panel</a> | <a href="doctorlogin.php">Doctor Login Panel</a></p>
-      <p class="fl_right">Developed by <a href="https://mhasan92542.wixsite.com/portfolio" target="_blank">Mohammad Mahmud Hasan</a></p>
+      <p class="fl_right">Developed by <a href="https://mhasan92542.wixsite.com/portfolio" target="_blank">Mohammad Mahmud Hasan and reviced by Suman Saha.</a></p>
       <br class="clear" />
     </div>
     <div class="clear"></div>
